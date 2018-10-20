044f9e1acce3eba9d6fb903403f5a01d69898cd8
Dynam1co <asensi.benito@gmail.com>
2018-10-20 13:52:00 +0200

    Download 3k images from earth in a loop

--- saveImagesLocally.php
+++ saveImagesLocally.php
@@ -2,63 +2,68 @@
 require 'gestionDatabase.php';
 
 $busqueda = 'earth';
+$page = 1;
 
-$curl = curl_init();
+for ($page = 1; $x <= 30; $page++) {
+    $strPage = strval($page);
 
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'http://localhost/api_fotos_nasa/index.php?searchWord=' . $busqueda . '&restultType=image&page=1',
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-));
+    $curl = curl_init();
 
-$resp = curl_exec($curl);
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'http://localhost/api_fotos_nasa/index.php?searchWord=' . $busqueda . '&restultType=image&page=' . $strPage,
+        CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+    ));
 
-curl_close($curl);
+    $resp = curl_exec($curl);
 
-$data = json_decode($resp);
+    curl_close($curl);
 
-foreach ($data->collection->items as $element) {
-    $object = json_decode(json_encode($element));
+    $data = json_decode($resp);
 
-    foreach ($object->links as $fin) {
-        $urlImagenNASA = $fin->href;
-        
-        $posInicioId = strpos($urlImagenNASA, 'image/') + 6;
-        $idImagen = substr($urlImagenNASA, $posInicioId);
-        $posFinId = strpos($idImagen, '/');
-        $idImagen = substr($idImagen, 0, $posFinId);
-        $nombreImagen = $idImagen . '.jpg';
-        $rutaCompleta = 'Images/' . $nombreImagen;
+    foreach ($data->collection->items as $element) {
+        $object = json_decode(json_encode($element));
 
-        file_put_contents($rutaCompleta, file_get_contents($urlImagenNASA));
+        foreach ($object->links as $fin) {
+            $urlImagenNASA = $fin->href;
+            
+            $posInicioId = strpos($urlImagenNASA, 'image/') + 6;
+            $idImagen = substr($urlImagenNASA, $posInicioId);
+            $posFinId = strpos($idImagen, '/');
+            $idImagen = substr($idImagen, 0, $posFinId);
+            $nombreImagen = $idImagen . '.jpg';
+            $rutaCompleta = 'Images/' . $nombreImagen;
 
-        $consulta = "INSERT INTO imagen (idNASA, nombreImagen, rutaCompleta, urlImagenNASA) VALUES (" . 
-            "'$idImagen', '$nombreImagen', '$rutaCompleta', '$urlImagenNASA'" .
-            ")";
+            file_put_contents($rutaCompleta, file_get_contents($urlImagenNASA));
 
-        $comando = Database::getInstance()->getDb()->prepare($consulta);
-		$comando -> execute();
-    }
+            $consulta = "INSERT INTO imagen (idNASA, nombreImagen, rutaCompleta, urlImagenNASA) VALUES (" . 
+                "'$idImagen', '$nombreImagen', '$rutaCompleta', '$urlImagenNASA'" .
+                ")";
 
-    foreach($object->data as $fin) {
-        $nasa_id = $fin->nasa_id;
-        $descImagen = $fin->description;
-        $descImagen = str_replace("'", "´", $descImagen);
-        $tipo = $busqueda;
-        $titulo = $fin->title;
-        $titulo = str_replace("'", "´", $titulo);
-        $fotografo = 'Unknown';
-
-        if (isset($fin->photographer)) {
-            $fotografo = $fin->photographer;
+            $comando = Database::getInstance()->getDb()->prepare($consulta);
+            $comando -> execute();
         }
 
-        $consulta = "UPDATE imagen SET " .
-            "descImagen = '$descImagen', tipo = '$tipo', tituloImagen = '$titulo', fotografo = '$fotografo' " .
-            "WHERE idNASA = '$nasa_id'";
+        foreach($object->data as $fin) {
+            $nasa_id = $fin->nasa_id;
+            $descImagen = $fin->description;
+            $descImagen = str_replace("'", "´", $descImagen);
+            $tipo = $busqueda;
+            $titulo = $fin->title;
+            $titulo = str_replace("'", "´", $titulo);
+            $fotografo = 'Unknown';
 
-        $comando = Database::getInstance()->getDb()->prepare($consulta);
-		$comando -> execute();
+            if (isset($fin->photographer)) {
+                $fotografo = $fin->photographer;
+            }
+
+            $consulta = "UPDATE imagen SET " .
+                "descImagen = '$descImagen', tipo = '$tipo', tituloImagen = '$titulo', fotografo = '$fotografo' " .
+                "WHERE idNASA = '$nasa_id'";
+
+            $comando = Database::getInstance()->getDb()->prepare($consulta);
+            $comando -> execute();
+        }
     }
 }
 ?>
