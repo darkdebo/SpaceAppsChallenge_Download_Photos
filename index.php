edd0959ca2b16d2718985851e73a8631e44d02b6
Dynam1co <asensi.benito@gmail.com>
2018-10-20 11:09:39 +0200

    Initial version

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php 
+ini_set('display_errors', 1);
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $searchWord = 'moon';
+    $restultType = 'image';
+    $page = '2';
+    
+    if (isset($_GET['searchWord'])) {
+        $searchWord = $_GET['searchWord'];
+    }
+    
+    if (isset($_GET['tipoResultado'])) {
+        $restultType = $_GET['tipoResultado'];
+    }
+
+    if (isset($_GET['page'])) {
+        $page = $_GET['page'];
+    }
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'https://images-api.nasa.gov/search?description=' . $searchWord . '&media_type=' . $restultType . '&page=' . $page,
+        CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+    ));
+
+    $resp = curl_exec($curl);
+
+    curl_close($curl);
+
+    echo $resp;
+}
+?>
